f9a95833bc5fe379704cd877a0a58cdf36bbcca9
DhirajVirani <87096857+DhirajVirani@users.noreply.github.com>
2022-03-15 00:40:44 -0400

    commit

--- src/Pages/QuoteDetails.js
+++ src/Pages/QuoteDetails.js
@@ -51,12 +51,12 @@ const QuoteDetail = () => {
       <HighLightedQuote text={loadedQuote.text} author={loadedQuote.author} />
       <Route path={match.path} exact>
         <div className="centered">
-          <Link className="btn--flat" to={`${match.url}/comments`}>
+          <Link className="btn--flat" to={`${match.url}comments`}>
             Load Comments
           </Link>
         </div>
       </Route>
-      <Route path={`${match.path}/comments`}>
+     <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
     </Fragment>
